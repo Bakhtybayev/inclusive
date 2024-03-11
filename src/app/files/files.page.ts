8b79689dc13450fece1252b6c40fecb54f182f68
Yeldar Bakhtybayev <yeldar.bakhtybayev@ntpayments.com>
2024-03-11 11:37:04 +0600

    cp

--- src/app/files/files.page.ts
+++ src/app/files/files.page.ts
@@ -11,13 +11,36 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './files.page.scss',
 })
 export class FilesPage implements OnInit {
-  files = ['one.pdf', 'two.pdf', 'three.pdf'];
-  file: any;
+  first_file: string = '';
   first_name: string = 'бағдарлама екі тілде';
+  second_file: string = '';
+  second_name: string =
+    '«Арнайы және инклюзивті білім беру жағдайында есту қабілеті зақымдалған балаларды оқытудың заманауи әдістері мен технологиялары»';
+  third_file: string = '';
+  third_name: string =
+    'Инклюзивті және арнайы білім беру жағдайында ерекше білім беру қажеттілігі бар балаларды оқыту мен тәрбиелеуді ұйымдастыру';
+  four_file: string = '';
+  four_name: string =
+    'Инклюзивті және арнайы білім беру жағдайында ерекше білім беру қажеттіліктері бар балаларды  мектепке дейінгі ұйымдарда оқыту және тәрбиелеуді ұйымдастыру';
 
   ngOnInit(): void {
     fetch('../../assets/files/бағдарлама екі тілде.pdf').then((res) => {
-      this.file = res.url;
+      this.first_file = res.url;
+    });
+    fetch(
+      '../../assets/files/«Арнайы және инклюзивті білім беру жағдайында есту қабілеті зақымдалған балаларды оқытудың заманауи әдістері мен технологиялары».pdf'
+    ).then((res) => {
+      this.second_file = res.url;
+    });
+    fetch(
+      '../../assets/files/Инклюзивті және арнайы білім беру жағдайында ерекше білім беру қажеттілігі бар балаларды оқыту мен тәрбиелеуді ұйымдастыру.pdf'
+    ).then((res) => {
+      this.third_file = res.url;
+    });
+    fetch(
+      '../../assets/files/Инклюзивті және арнайы білім беру жағдайында ерекше білім беру қажеттіліктері бар балаларды  мектепке дейінгі ұйымдарда оқыту және тәрбиелеуді ұйымдастыру.pdf'
+    ).then((res) => {
+      this.four_file = res.url;
     });
   }
 }
